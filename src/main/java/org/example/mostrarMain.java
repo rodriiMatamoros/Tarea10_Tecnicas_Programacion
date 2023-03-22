879950109af32b8b934e2ba7b7beeb0439ce4aee
Rodrigo Matamoros Becerra <rodri@MacBook-Pro-de-Rodrigo.local>
2023-03-22 20:34:09 +0100

    commit raizCuadrada

--- src/main/java/org/example/mostrarMain.java
+++ src/main/java/org/example/mostrarMain.java
@@ -1,4 +1,6 @@
-package org.example;
+/*package org.example;
 
 public class mostrarMain {
+
 }
+*/
--- src/main/java/org/example/raizCuadrada.java
+++ src/main/java/org/example/raizCuadrada.java
@@ -1,4 +1,28 @@
 package org.example;
+import java.util.Scanner;
 
 public class raizCuadrada {
+
+    // Definimos la función que calcula la raíz cuadrada y lanza una excepción si su argumento es negativo
+    public static double raizCuadrada(double numero) throws Exception {
+        if (numero < 0) {
+            throw new Exception("La raíz cuadrada no está definida para un número negativo.");
+        }
+        return Math.sqrt(numero);
+    }
+
+    // Función main para la entrada y visualización
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introducir un valor: ");
+        double numero = scanner.nextDouble();
+        scanner.close();
+
+        try {
+            double resultado = raizCuadrada(numero);
+            System.out.println("√" + numero + " = " + resultado);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
